6dd394e79a38195753a1e13bde62a924d96dd922
angel <angelriera1796@gmail.com>
2022-06-18 15:09:25 -0400

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// npx tailwindcss init --full
+// npx tailwindcss -i ./input.css -o ./all-tailwind.css --watch
+// npx tailwindcss build ./input.css -o ./all-tailwind.css
