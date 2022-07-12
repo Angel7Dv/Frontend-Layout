98787fa71566ea2963dbdd267bb2190ce56d0835
angel <angelriera1796@gmail.com>
2022-07-12 19:25:36 -0400

    animations on underline

--- src/art-portfolio/app.js
+++ src/art-portfolio/app.js
@@ -1,36 +1,75 @@
+const $ = (e) => document.querySelector(e)
 gsap.registerPlugin(ScrollTrigger)
 
-const feats = gsap.timeline({
-    repeat:0,
-    scrollTrigger:{
+// Hero
+gsap.timeline({
+    repeat: 0,
+    scrollTrigger: {
         trigger: '#hero',
         start: 'top top',
         scrub: 3
     }
-    
+}).from('#hero-img', {
+    duration: 1,
+    y: 40,
+    delay: 0,
+    opacity: 1
+}).to('#hero-text', {
+    duration: 1,
+    y: 10,
+    delay: 0,
+    opacity: 1
+}, '-=1').from('#hero-text-2', {
+    duration: 1,
+    y: 250,
+    delay: 0,
+    opacity: 1
+}, '-=1')
+
+
+// hover
+
+let marco = $('.img-marc')
+let zoom = gsap.timeline({
+    repeat: 0,
+}).to('.img', {
+    duration: 1,
+    scaleX: 1.2,
+    scaleY: 1.2
+}).from(marco, {
+    duration: 1,
+    scaleX: 1.1,
+    scaleY: 1.1
+}, '-=1')
+
+marco.addEventListener("mouseover", () => zoom.play());
+marco.addEventListener("mouseout", () => zoom.reverse());
+
+
+
+const imgtitle = $('#img-title')
+
+const underline = gsap.timeline({
+    repeat: 0,
+}).to(imgtitle, {
+    duration: 2,
+    textDecoration: ""
 })
 
-feats.from('#hero-img',{
-    duration: 1,
-    y:40,
-    delay: 0,
-    opacity: 1
-})
-feats.to('#hero-text',{
-    duration: 1,
-    y:10,
-    delay: 0,
-    opacity: 1
-},'-=1')
-feats.from('#hero-text-2',{
-    duration: 1,
-    y:250,
-    delay: 0,
-    opacity: 1
-},'-=1')
+imgtitle.addEventListener("mouseover", () => underline.play());
+imgtitle.addEventListener("mouseout", () => underline.reverse());
 
 
 
+
+
+/**
+ *     
+    {
+        scaleX: 1 ,
+        scaleY: 1
+    },
+ * / 
 
 // feats.from('#feat_1',{
 //     duration: 1,
